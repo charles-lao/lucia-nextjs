c27a0a253e274f10f2583fd339f659c6a05beb38
charles-lao <charles.h.lao@gmail.com>
2024-05-22 16:07:13 +1200

    add logout function

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,9 +1,10 @@
 'use server';
 
 import { redirect } from 'next/navigation';
-import { hashUserPassword } from "@/lib/hash";
-import { createUser } from "@/lib/user";
-import { createAuthSession } from '@/lib/auth';
+import { hashUserPassword, verifyPassword } from "@/lib/hash";
+import { createUser, getUserByEmail } from "@/lib/user";
+import { createAuthSession, destroySession } from '@/lib/auth';
+
 
 
 export async function signup(prevState, formData) {
@@ -43,4 +44,47 @@ export async function signup(prevState, formData) {
     }
 
     
+}
+
+
+export async function login(prevState, formData) {
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+     const existingUser = getUserByEmail(email);
+
+     if (!existingUser) {
+        return {
+            errors: {
+                email: 'Could not authenticate user, please check your credentials.'
+            },
+        };
+     }
+
+
+     const isValidPassword = verifyPassword(existingUser.password, password);
+
+     if (!isValidPassword) {
+        return {
+            errors: {
+                password: 'Could not authenticate user, please check your credentials.'
+            },
+        };
+     }
+
+
+    await createAuthSession(existingUser.id);
+    redirect('/training');
+}
+
+export async function auth(mode, prevState, formData) {
+    if (mode === 'login') {
+        return login(prevState, formData);
+    }
+    return signup(prevState, formData);
+}
+
+export async function logout() {
+    await destroySession();
+    redirect('/');
 }
---
+++ app/(auth)/layout.js
@@ -0,0 +1,23 @@
+import { logout } from '@/actions/auth-actions';
+import '../globals.css';
+
+export const metadata = {
+  title: 'Next Auth',
+  description: 'Next.js Authentication',
+};
+
+export default function AuthRootLayout({ children }) {
+  return (
+    <html lang="en">
+        <body>
+            <header id="auth-header">
+                <p>Welcome back!</p>
+                <form action={logout}>
+                    <button>Logout</button>
+                </form>
+            </header>
+            {children}
+        </body>
+    </html>
+  );
+}
